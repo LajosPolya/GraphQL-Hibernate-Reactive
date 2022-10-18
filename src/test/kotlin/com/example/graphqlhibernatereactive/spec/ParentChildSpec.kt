8a2945db55f1f4bb381e0193c076146fe408cd57
LajosPolya <lajospolya94@gmail.com>
2022-10-18 11:42:04 -0400

    add tests

---
+++ src/test/kotlin/com/example/graphqlhibernatereactive/spec/ParentChildSpec.kt
@@ -0,0 +1,75 @@
+package com.example.graphqlhibernatereactive.spec
+
+import com.example.graphqlhibernatereactive.entity.Parent
+import com.netflix.graphql.dgs.client.MonoGraphQLClient
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.web.reactive.function.client.WebClient
+import java.util.concurrent.CompletionException
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+internal class ParentChildSpec(@LocalServerPort port: Int) {
+    private lateinit var monoGraphQLClient: MonoGraphQLClient
+    val webClient = WebClient.create("http://localhost:$port/graphql")
+
+    /**
+     * This test populates the database then
+     * fetches the Parents' Children using withSession.
+     */
+    @Test
+    fun parentsChildrenWithSession() {
+        monoGraphQLClient = MonoGraphQLClient.createWithWebClient(webClient) {
+                headers -> headers.add("withSession", "true")
+        }
+
+        // populate DB with data
+        val mutation = "mutation { createData }"
+        var response = monoGraphQLClient.reactiveExecuteQuery(mutation).block()
+        assertTrue(response!!.extractValue<Boolean>("createData"))
+
+        val query = "{ parents { name children { name } }}"
+        response = monoGraphQLClient.reactiveExecuteQuery(query).block()
+        val parents: List<*>? = response?.extractValueAsObject(
+            "parents[*]",
+            List::class.java
+        )
+
+        parents?.forEach {
+            val children = (it as LinkedHashMap<*, *>)["children"]
+            assertNotNull(children)
+            assertNotEquals((children as List<*>).size, 0)
+        }
+    }
+
+    /**
+     * This test populates the database then
+     * fetches the Parents' Children using withTransaction
+     */
+    @Test
+    fun parentsChildrenWithTransaction() {
+        monoGraphQLClient = MonoGraphQLClient.createWithWebClient(webClient)
+
+        // populate DB with data
+        val mutation = "mutation { createData }"
+        var response = monoGraphQLClient.reactiveExecuteQuery(mutation).block()
+        assertTrue(response!!.extractValue<Boolean>("createData"))
+
+        val query = "{ parents { name children { name } }}"
+        response = monoGraphQLClient.reactiveExecuteQuery(query).block()
+        val parents: List<*>? = response?.extractValueAsObject(
+            "parents[*]",
+            MutableList::class.java
+        )
+
+        parents?.forEach {
+            val children = (it as LinkedHashMap<*, *>)["children"]
+            assertNotNull(children)
+            assertNotEquals((children as List<*>).size, 0)
+        }
+    }
+}
